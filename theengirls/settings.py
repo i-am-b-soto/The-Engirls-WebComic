97541e37de545bec6d656d4acc50208e40eab5d0
iamsoto <iambriansoto@gmail.com>
2020-02-26 08:08:04 -0800

    Getting Tired of Herokus BS

--- theengirls/settings.py
+++ theengirls/settings.py
@@ -143,13 +143,14 @@ WSGI_APPLICATION = 'theengirls.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+"""
 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
