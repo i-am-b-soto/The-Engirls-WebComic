2e3ef99f13094d5ad68951e628828a84d8d908ee
iamsoto <iambriansoto@gmail.com>
2020-02-26 08:41:42 -0800

    Bullshit Commit to try and fix heroku

--- comics/urls.py
+++ comics/urls.py
@@ -5,9 +5,9 @@ from . import views
 app_name = "comics"
 urlpatterns = [
      path('', views.index, name='comic_index')
-    ,path('<int:comic_pk>/',views.view_panel, name ='view_panel')
-    ,path('archive/', views.view_archive, name = 'view_archive')
-    ,path('comments/<int:comic_pk>/<int:page>/', views.view_comments, name = 'view_comments')
+    #,path('<int:comic_pk>/',views.view_panel, name ='view_panel')
+    #,path('archive/', views.view_archive, name = 'view_archive')
+    #,path('comments/<int:comic_pk>/<int:page>/', views.view_comments, name = 'view_comments')
     
 ]
 
